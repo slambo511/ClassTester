e623aeb933b054af3c2b6ef8210472ae4a2b3ce8
Samuel Bancroft <samuelbancroft@hotmail.com>
2016-10-11 15:19:45 +0100

    added XML markup

--- ClassTester/RegistryEditor.cs
+++ ClassTester/RegistryEditor.cs
@@ -5,18 +5,48 @@ using Microsoft.Win32;
 namespace ClassTester
 {
     /// <summary>
+    /// Import class with using statement and instantiate in custom fashion i.e. RegistryEditor editor = new RegistryEditor();
+    /// The initial three properties have these default values but can be modified (in code) to suit:
+    /// Property                Default Value                           Example
+    /// _showError              true                                    editor._showError = false;
+    /// _baseRegistryKey        Registry.LocalMachine                   editor._baseRegistryKey = Registry.CurrentUser;
+    /// _subKey                 "SOFTWARE\\" + Application.ProductName  editor._subKey = "SOFTWARE\\MYPROGRAM\\MYSUBKEY";
+    /// The code can of course be altereed to add getters and setters for these properties to make the amendable by the calling code.
     /// 
-    /// </summary>
+    /// Examples:
+    /// 
+    /// Read:               editor.Read("MY_KEY");
+    /// Write:              editor.Write("MY_KEY", "MY_VALUE");
+    /// Delete single:      editor.Delete("MY_KEY");
+    /// Delete subkey tree: editor.DeleteSubKeyTree();
+    /// Count subkeys:      editor.SubKeyCount();
+    /// Count values inkey: editor.ValueCount(); 
+    ///
+    /// ...
+    /// RegistryEditor myRegistry = new RegistryEditor();
+    /// myRegistry.SubKey = "SOFTWARE\\RTF_SHARP_EDIT\\RECENTFILES";
+    /// myRegistry.ShowError = true;
+    /// int numberValues = myRegistry.ValueCount();
+    /// loop
+    /// arrayRecentFiles[i] = myRegistry.Read(i.ToString());
+    /// ...
+    /// 
+    /// ...
+    /// RegistryEditor myRegistry = new RegistryEditor();
+    /// myRegistry.SubKey = "SOFTWARE\\APP_NAME\\RECENTFILES";
+    /// myRegistry.ShowError = true; // if getter setter bound
+    /// myRegistry.DeleteSubKeyTree();
+    /// loop
+    /// if (arrayRecentFiles[i] == null)
+    ///     break;
+    /// myRegistry.Write(i.ToString(), arrayRecentFiles[i]);
+    /// ...
+    ///  </summary>
     class RegistryEditor
     {
         private readonly bool _showError = true;
         private readonly RegistryKey _baseRegistryKey = Registry.LocalMachine;
-        private readonly string _subKey;
-
-        public RegistryEditor(RegistryKey baseRegistryKey, string subKey)
-        {
-            _subKey = subKey;
-        }
+        private readonly string _subKey = "SOFTWARE\\" + Application.ProductName;
 
         // Read function
         public string Read(string keyName)
